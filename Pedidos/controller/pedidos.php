473cbbac0c9821de09996d949bba270aacb8ff61
YisselZavala <ericka.flores@unah.hn>
2021-11-21 19:24:23 -0600

    Modificación de ruta de la Carpeta Pedidos

--- Pedidos/controller/pedidos.php
+++ Pedidos/controller/pedidos.php
@@ -11,8 +11,8 @@ if ( $_SERVER [ 'REQUEST_METHOD' ] === 'OPTIONS' ) {
     header ( 'Access-Control-Allow-Origin: *' ); 
     header ( 'Content-Type: application/json' );
     
-     require_once("../config/conexion.php");
-     require_once("../models/Pedidos.php");
+     require_once("../../config/conexion.php");
+     require_once("../../Pedidos/models/Pedidos.php");
      $pedidos = new Pedidos();
 
      $body = json_decode(file_get_contents("php://input"), true);
